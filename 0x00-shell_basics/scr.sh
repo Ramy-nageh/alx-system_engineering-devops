8cd66b60ed4c07b4f4d17f5cc227ee89ce749a52
ramy nageh <ramy-nageh@live.com>
2023-03-08 12:51:16 -0800

    All my work

--- 0x00-shell_basics/scr.sh
+++ 0x00-shell_basics/scr.sh
@@ -1,54 +1,21 @@
 #!/bin/bash
 echo "#!/bin/bash
-cd~" > 2-bring_me_home
-chmod u+x 2-bring_me_home
+mv [[:upper:]]* /tmp/u" > 100-lets_move
+chmod u+x 100-lets_move
 
 echo "#!/bin/bash
-ls -l" > 3-listfiles
-chmod u+x 3-listfiles
+rm *~" > 101-clean_emacs
+chmod u+x 101-clean_emacs
 
 echo "#!/bin/bash
-ls -al" > 4-listmorefiles
-chmod u+x 4-listmorefiles
+mkdir -p welcome/to/school" > 102-tree
+chmod u+x 102-tree
 
 echo "#!/bin/bash
-ls -lna" > 5-listfilesdigitonly
-chmod u+x 5-listfilesdigitonly
+ls -xamp" > 103-commas
+chmod u+x 103-commas
 
-echo "#!/bin/bash
-mkdir /tmp/my_first_directory/" > 6-firstdirectory
-chmod u+x 6-firstdirectory
-
-echo "#!/bin/bash
-mv /tmp/berry /tmp/my_first_directory" > 7-movethatfile 
-chmod u+x 7-movethatfile
-
-echo "#!/bin/bash
-rm /tmp/my_first_directory/betty" > 8-firstdelete
-chmod u+x 8-firstdelete
-
-echo "#!/bin/bash
-rm -rf /tmp/my_first_directory" > 9-firstdirdeletion
-chmod u+x 9-firstdirdeletion
-
-echo "#!/bin/bash
-cd .." > 10-back
-chmod u+x 10-back
-
-echo "#!/bin/bash
-ls -al . .. /boot" > 11-lists
-chmod u+x 11-lists
-
-echo "#!/bin/bash
-file /tmp/iamfile" > 12-file_type
-chmod u+x 12-file_type
-
-echo "#!/bin/bash
-ln -s /bin/ls __ls__" > 13-symbolic_link 
-chmod u+x 13-symbolic_link
-
-echo "#!/bin/bash
-cp -un *.html ../" > 14-copy_html
-chmod u+x 14-copy_html
-
+echo "0 string HOLBERTON Holberton data
+!:mime Holberton" > school.mgc
+chmod u+x school.mgc
 #end
